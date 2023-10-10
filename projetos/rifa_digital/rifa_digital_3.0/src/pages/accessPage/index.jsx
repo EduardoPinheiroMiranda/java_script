3a1cf988aeab79bd8d0ba03bbba447f5a70d4ae7
Eduardo <pinheiomiranda@gmail.com>
2023-10-10 10:18:43 -0300

    criação da página de login, 70% pronta

--- projetos/rifa_digital/rifa_digital_3.0/src/pages/accessPage/index.jsx
+++ projetos/rifa_digital/rifa_digital_3.0/src/pages/accessPage/index.jsx
@@ -1,4 +1,4 @@
-import { Container, LogIn, SingIn } from './style'
+import { Container, Information, SignUp, LogIn } from './style'
 
 import { Input } from '../../components/input'
 import { Button } from '../../components/Button'
@@ -8,23 +8,40 @@ export function AccessPage(){
     return(
         <Container>
             <main>
-                <LogIn>
+                <form>
                     <h2>Entrar</h2>
                     <Input placeholder='Email'/>
                     <Input placeholder='Senha'/>
                     <Button title='Entrar'/>
-                </LogIn>
+                </form>
 
-                <SingIn>
+                <form>
                     <h2>Criar conta</h2>
                     <Input placeholder='Nome'/>
                     <Input placeholder='Email'/>
                     <Input placeholder='Senha'/>
                     <Button title='Cadastrar'/>
-                </SingIn>
+                </form>
 
-                <Information>
-                    
+                <Information className='botton'>
+                    <LogIn>
+                        <h2>Que bom que voltou !</h2>
+                        <p>
+                            Insira suas informações de login e venha 
+                            ser um vencedor. 
+                        </p>
+                        <span>ou</span>
+                        <Button title='cria conta'/>
+                    </LogIn>
+
+                    <SignUp className='hide'>
+                        <h2>Seja bem-vindo!</h2>
+                        <p>
+                            Cadastre-se em nossa plataforma e venha ser vitorioso.
+                        </p> 
+                        <span>ou</span>
+                        <Button title='Já tenho conta'/>
+                    </SignUp>
                 </Information>
             </main>
         </Container>
--- projetos/rifa_digital/rifa_digital_3.0/src/pages/accessPage/style.js
+++ projetos/rifa_digital/rifa_digital_3.0/src/pages/accessPage/style.js
@@ -15,10 +15,22 @@ export const Container = styled.div`
         background-color: ${({ theme }) => theme.colors.white};
         width: 30rem;
 
-        border-radius: 0.3rem;
+        border-radius: 0.5rem;
 
         display: grid;
         gap: 3rem;
+
+        position: relative;
+
+        .top{
+            position: absolute;
+            top: 0;
+        }
+
+        .botton{
+            position: absolute;
+            bottom: 0;
+        }
     }
 
     > main form{
@@ -50,12 +62,50 @@ export const Container = styled.div`
 
 `
 
-export const LogIn = styled.form`
+export const Information = styled.div`
 
+    >div{
+        background-color: ${({ theme }) => theme.colors.blue};
+        color: ${({ theme }) => theme.colors.white};
+
+        width: 100%;
+        height: 31rem;
+        border-radius: 0.5rem;
+
+        display: flex;
+        flex-direction: column;
+
+        justify-content: center;
+        align-items: center;
+
+
+        h2{
+            font-size: 2.5rem;
+            margin-bottom:3rem
+        }
+
+        p{
+            font-size:1.6rem;
+            text-align: center;
+            padding: 0 1rem;
+            margin-bottom: 3rem;
+        }
+
+        span{
+            font-size:1.4rem;
+            margin-bottom: 3rem;
+        }
+    }
+
+    .hide{
+        display: none;
+    }
+
+`
+export const LogIn = styled.div`
     
 
 `
-
-export const SingIn = styled.form`
+export const SignUp = styled.div`    
 
 `
