75430a18ca5fbb125ce2686915df99f51c68a9f4
Eduardo Pinheiro Miranda <89249898+EduardoPinheiroMiranda@users.noreply.github.com>
2024-01-03 17:36:42 -0300

    criação de rota de login funcional

--- projetos/Color_colletcion/backend/src/routers/user.router.ts
+++ projetos/Color_colletcion/backend/src/routers/user.router.ts
@@ -5,8 +5,27 @@ import { z } from "zod"
 import { conexaoDoBanco as knex} from "../dataBase/ConexaoDoBanco"
 
 export async function user( app: FastifyInstance){
-	app.get("/", (request, replay) => {
-		return replay.status(201).send
+	app.post("/login", async (request, replay) => {
+
+		const interfaceValidaDeLogin = z.object({
+			email: z.string(),
+			password: z.string()
+		})
+
+		const {email, password} = interfaceValidaDeLogin.parse(request.body)
+
+		const user = await knex("users").where("email", email).first()
+
+		if(!user){
+			throw new Error("Usuário ou senha invalidos")
+		}
+
+		if(user.password !== password){
+			throw new Error("Usuário ou senha invalidos")
+		}
+        
+		return replay.status(201).send()
+
 	})
 
 	app.post("/cadastrar", async (request, replay) => {
