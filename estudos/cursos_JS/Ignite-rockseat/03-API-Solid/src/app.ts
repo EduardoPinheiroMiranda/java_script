96fae450a5d76f3bf1324f17abc666f161c2be27
Eduardo Pinheiro Miranda <89249898+EduardoPinheiroMiranda@users.noreply.github.com>
2024-01-09 22:20:24 -0300

    add o prisma ao projeto :

--- estudos/cursos_JS/Ignite-rockseat/03-API-Solid/src/app.ts
+++ estudos/cursos_JS/Ignite-rockseat/03-API-Solid/src/app.ts
@@ -1,3 +1,13 @@
-import fastif from "fastify";
+import fastif from "fastify"
+import { PrismaClient } from "@prisma/client"
 
 export const app = fastif()
+
+const prisma = new PrismaClient()
+
+prisma.user.create({
+	data: {
+		name: "eduardo",
+		email: "eduardo@teste.com.br"
+	}
+})
