e6fa2fce92c2106cded10ef8a51a30e9d3f162fa
Eduardo Pinheiro Miranda <89249898+EduardoPinheiroMiranda@users.noreply.github.com>
2024-01-14 23:39:00 -0300

    Update app.ts
    Formatando estrutura

--- estudos/cursos_JS/Ignite-rockseat/03-API-Solid/src/app.ts
+++ estudos/cursos_JS/Ignite-rockseat/03-API-Solid/src/app.ts
@@ -3,8 +3,6 @@ import fastify from "fastify"
 import { registerRouter } from "./http/routers/register_router" 
 import { errorsGlobal } from "./Errors/Global"
 
-
-
 export const app = fastify()
 
 app.register(registerRouter)
